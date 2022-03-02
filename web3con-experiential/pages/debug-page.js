419bd0c740826d809ca840c1c6949227c2413f11
Robert336 <rmazza33@gmail.com>
2022-03-01 21:36:34 -0500

    create debug page

--- web3con-experiential/pages/debug-page.js
+++ web3con-experiential/pages/debug-page.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import styles from "../styles/ARview.module.css";
+import styles from "../styles/debug-page.module.css";
 import Link from "next/link";
 
 // Calling it ARview for now, this is going to be the main page of our app.
@@ -45,7 +45,7 @@ export default function ARview() {
     return (
         <div className={styles.container}>
             <h1>
-                AR View
+                Debug Page (for testing components)
             </h1>
             <p>Current Account: {currentAccount}</p>
             <h3>
